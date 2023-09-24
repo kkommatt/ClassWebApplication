52875d87e29af9edbfc8b9f0701b0ed92bc7016a
Matthew Kovalyk <matviigame@gmail.com>
2023-09-24 14:15:39 +0300

    Added models

---
+++ ClassWebApplication/Models/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ClassWebApplication.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Lector> Lectors { get; set; }
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+        public virtual DbSet<LectorCourse> LectorCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentCourses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentCourses)
+                .HasForeignKey(sc => sc.CourseId);
+
+            modelBuilder.Entity<LectorCourse>()
+                .HasKey(lc => new { lc.LectorId, lc.CourseId });
+
+            modelBuilder.Entity<LectorCourse>()
+                .HasOne(lc => lc.Lector)
+                .WithMany(l => l.LectorCourses)
+                .HasForeignKey(lc => lc.LectorId);
+
+            modelBuilder.Entity<LectorCourse>()
+                .HasOne(lc => lc.Course)
+                .WithMany(c => c.LectorCourses)
+                .HasForeignKey(lc => lc.CourseId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+}
--- ClassWebApplication/Program.cs
+++ ClassWebApplication/Program.cs
@@ -1,7 +1,14 @@
+using ClassWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(
+builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 var app = builder.Build();
 
