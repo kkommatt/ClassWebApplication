ca8ec2cebfbf70c1f95b4e81490c5372e87a7462
Matthew Kovalyk <matviigame@gmail.com>
2023-09-24 19:00:41 +0300

    Added controllers and views

---
+++ ClassWebApplication/Controllers/LectorsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClassWebApplication.Models;
+
+namespace ClassWebApplication.Controllers
+{
+    public class LectorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LectorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lectors
+        public async Task<IActionResult> Index()
+        {
+              return _context.Lectors != null ? 
+                          View(await _context.Lectors.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Lectors'  is null.");
+        }
+
+        // GET: Lectors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Lectors == null)
+            {
+                return NotFound();
+            }
+
+            var lector = await _context.Lectors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lector == null)
+            {
+                return NotFound();
+            }
+
+            return View(lector);
+        }
+
+        // GET: Lectors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lectors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FullName,Email,University")] Lector lector)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lector);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lector);
+        }
+
+        // GET: Lectors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Lectors == null)
+            {
+                return NotFound();
+            }
+
+            var lector = await _context.Lectors.FindAsync(id);
+            if (lector == null)
+            {
+                return NotFound();
+            }
+            return View(lector);
+        }
+
+        // POST: Lectors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,Email,University")] Lector lector)
+        {
+            if (id != lector.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lector);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LectorExists(lector.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lector);
+        }
+
+        // GET: Lectors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Lectors == null)
+            {
+                return NotFound();
+            }
+
+            var lector = await _context.Lectors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lector == null)
+            {
+                return NotFound();
+            }
+
+            return View(lector);
+        }
+
+        // POST: Lectors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Lectors == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Lectors'  is null.");
+            }
+            var lector = await _context.Lectors.FindAsync(id);
+            if (lector != null)
+            {
+                _context.Lectors.Remove(lector);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LectorExists(int id)
+        {
+          return (_context.Lectors?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
