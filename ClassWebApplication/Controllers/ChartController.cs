2d1dda37736dfab7d3f97270764b37d4ff2f17e1
Matthew Kovalyk <matviigame@gmail.com>
2023-11-11 19:44:30 +0200

    feat: add vizualization of statistics

---
+++ ClassWebApplication/Controllers/ChartController.cs
@@ -0,0 +1,46 @@
+﻿using ClassWebApplication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("countStudents")]
+        public JsonResult GetCountStudents()
+        {
+            var coursesData = _context.Courses
+                .Select(course => new
+                {
+                    label = course.Title,
+                    value = course.StudentCourses.Count 
+                })
+                .ToList();
+
+            return new JsonResult(coursesData);
+        }
+
+        [HttpGet("lecturersData")]
+        public JsonResult GetLecturersData()
+        {
+            var lecturersData = _context.Lectors
+                .Select(lector => new
+                {
+                    lecturer = lector.FullName,
+                    coursesCount = lector.LectorCourses.Count 
+                })
+                .ToList();
+
+            return new JsonResult(lecturersData);
+        } 
+    }
+}
